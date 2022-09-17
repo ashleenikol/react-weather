5d8de799e7f8e9f4f26f12cdf8401f1e6ee8e9f6
Ashlee Bartley <80643645+ashleenikol@users.noreply.github.com>
2022-09-17 15:28:57 -0500

    Added row & column CSS.

--- src/Header.js
+++ src/Header.js
@@ -9,17 +9,17 @@ export default function Header() {
       <h2 id="date-time">Thursday, 12:00PM</h2>
       <h2>Sunny</h2>
       <div className="row">
-        <div className="col">
+        <div className="col-6">
           <img src={pic} alt="sunny" width="100" />
           <h2 id="temp"> 15°F</h2>
         </div>
-        <div className="col">
+        <div className="col-6">
           <ul className="description">
             <li>
-              Humidity: <span id="humidity"></span>%
+              Humidity: <span id="humidity">30</span>%
             </li>
             <li>
-              Wind: <span id="wind"></span>mph
+              Wind: <span id="wind">5</span>mph
             </li>
           </ul>
         </div>
