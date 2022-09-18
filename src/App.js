3b96c723e05fb884315656785983b54d57a7b0d5
Ashlee Bartley <80643645+ashleenikol@users.noreply.github.com>
2022-09-17 19:57:15 -0500

    Started Forecast

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Search from "./Search";
 import Header from "./Header";
+import Forecast from "./Forecast";
 
 function App() {
   return (
     <div className="Weather">
       <Search />
       <Header />
+      <Forecast />
     </div>
   );
 }
